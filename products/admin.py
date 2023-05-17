cfb58728c734c487652f124c4337571bdfa694f7
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-05-17 23:59:30 +0300

    feat: ModelAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Product
 
+class ProductAdmin(admin.ModelAdmin):
+    pass
 
-admin.site.register(Product)
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.site_title = "My Admin Title"
+admin.site.site_header = "My Admin Portal"
+admin.site.index_title = "Welcome to My Admin Portal"
