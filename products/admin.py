c8948f442f0ceb2aa59452bb628a9a1907b5b115
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-05-20 01:08:47 +0300

    feat: add ReviewInline

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Product, Review
 from django.utils import timezone
 
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+    classes = ('collapse',)
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_display = ("name", "create_date", "is_in_stock", "update_date", "added_days_ago", "how_many_reviews")
     list_editable = ("is_in_stock",)
     list_filter = ("is_in_stock", "create_date")
     ordering = ("name",)
@@ -11,6 +16,7 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug" : ("name",)}
     list_per_page = 25
     date_hierarchy = "update_date"
+    inlines = (ReviewInline,)
     # fields = (("name","slug"), "description","is_in_stock")
     fieldsets = (
         (None, {
@@ -38,11 +44,24 @@ class ProductAdmin(admin.ModelAdmin):
     def added_days_ago(self, product):
         difference = timezone.now() - product.create_date
         return difference.days
+    
+    def how_many_reviews(self, obj):
+        count = obj.reviews.count()
+        return count
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'created_date', 'is_released')
+    list_per_page = 50
+    raw_id_fields = ('product',) 
+    # # list_filter = ("product",)
+    # list_filter = (
+    #      ('product', RelatedDropdownFilter),
+    # )
+    # resource_class = ReviewResource
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 
 admin.site.site_title = "My Admin Title"
 admin.site.site_header = "My Admin Portal"
